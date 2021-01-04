003ab1c85797847c43d8ad6a94b9956c5f838a90
Natalia Baggi <natibaggi@gmail.com>
2021-01-04 11:59:43 -0300

    Criando NavBar Backoffice

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
+  namespace :backoffice do
+    get '/', to: 'home#index'
+
+    resources(:companies)
+  end
+
   defaults({format: :json}) do
     get('/:company_id', {to: "company#index", as: "company_front_web"})
   end
