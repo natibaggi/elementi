5afdbe776b6ef72ce96d987d61fab151b9c43b70
Natalia Baggi <natibaggi@gmail.com>
2021-01-04 22:24:30 -0300

    Atualização do Create - Empresas

--- app/controllers/backoffice/companies_controller.rb
+++ app/controllers/backoffice/companies_controller.rb
@@ -1,5 +1,4 @@
 class Backoffice::CompaniesController < ApplicationController
-
     layout('backoffice')
 
     def index
@@ -7,7 +6,20 @@ class Backoffice::CompaniesController < ApplicationController
     end
 
     def new
-
+        @company = Company.new(status: "Active")
     end
 
+    def create        
+        company_params = params.require(:company).permit(:cnpj, :name, :status)
+
+        @company = Company.new(company_params)
+        if @company.save
+            flash[:notice] = "Empresa salva com sucesso!"
+            redirect_to backoffice_companies_path
+        else
+            flash.now[:error] = @company.errors.full_messages
+            render :new
+        end
+        
+    end
 end
--- app/models/company.rb
+++ app/models/company.rb
@@ -1,4 +1,6 @@
 class Company < ApplicationRecord
+    validates :name, presence: true
+
     has_one :catalog
     has_many(:categories)
 end
