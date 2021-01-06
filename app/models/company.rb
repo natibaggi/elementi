e6f135d92e7d735e1d8303767c534043693edb8a
Natalia Baggi <natibaggi@gmail.com>
2021-01-06 11:44:20 -0300

    Validação de CNPJ

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,6 +1,16 @@
 class Company < ApplicationRecord
     validates :name, presence: true
+    validate :cnpj_validates
 
     has_one :catalog
     has_many(:categories)
+
+
+    private
+    def cnpj_validates
+        if !CNPJ.valid?(cnpj)
+            errors.add(:cnpj, " não é válido")
+        end
+    end
+
 end
