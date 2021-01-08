a1329adc2dafa81ef53e973e056e84ae89bf30fb
Natalia Baggi <natibaggi@gmail.com>
2021-01-08 14:45:44 -0300

    Configurando a Gem shoulda-matchers

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -62,3 +62,11 @@ RSpec.configure do |config|
   # including factory_bot for specs
   config.include FactoryBot::Syntax::Methods
 end
+
+
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
+end
