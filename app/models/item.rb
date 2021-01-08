4e05a24058d872b342a4f330369e083c8eac7f99
Natalia Baggi <natibaggi@gmail.com>
2021-01-08 16:26:36 -0300

    Validações e Testes de Itens

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,11 @@
 class Item < ApplicationRecord
   belongs_to :catalog
   belongs_to :category
+
+  validates :name, length: { minimum: 3 }
+  validates :price, numericality: {greater_than: 0}
+    
+  validates :description, length: { minimum: 6 }
+  validates :status, inclusion: { in: ["active", "inactive"]}
+
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe 'associations' do
+    it { should belong_to(:catalog) }
+    it { should belong_to(:category)}
+  end
+
+  describe 'validations' do
+      it { should validate_length_of(:name).is_at_least(3) }
+      it { should validate_numericality_of(:price).is_greater_than(0) }
+      it { should validate_length_of(:description).is_at_least(6) }
+      it {should validate_inclusion_of(:status).in_array(["active", "inactive"])}
+  end
+
+
 end
